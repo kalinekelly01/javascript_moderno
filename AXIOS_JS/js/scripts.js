50f079946a2652cf6ef1286c4c4641e428f044fa
Kaline Kelly <145710571+kalinekelly01@users.noreply.github.com>
2023-12-03 09:22:14 -0300

    Axios JS

---
+++ AXIOS_JS/js/scripts.js
@@ -0,0 +1,16 @@
+// 1 - instalação
+console.log(axios);
+
+// 2 - primeiro request
+const getData = async() => {
+
+    try {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+        console.log(response);
+        return response.data;
+        
+    } catch (error) {
+        console.log(error);
+    }
+};
+getData();
--- JS_MODERNO/js/scripts.js
+++ JS_MODERNO/js/scripts.js
@@ -121,3 +121,39 @@ const carbrand = {brand: "VW"};
 const otherInfos = {km: 1000000, price: 3090999};
 const car = {...carName, ...carbrand, ...otherInfos};
 console.log(car);
+
+// 8 - classes
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    productWithDiscount(discount) {
+        return this.price * ((100 - discount) / 100);
+    }
+}
+
+const shirt = new Product("Camisa gola V", 20);
+console.log(shirt);
+console.log(shirt.productWithDiscount(10));
+
+// 9 - herança
+class ProductWithAttributes extends Product {
+    constructor(name, price, colors) {
+        super(name, price);
+        this.colors = colors;
+    }
+
+    showColors() {
+        console.log("As cores são: ");
+        this.colors.forEach((color) => {
+            console.log(color);
+        });
+    }
+}
+const hat = new ProductWithAttributes("Chapéu", 25.99, ["Preto", "Rosa", "Azul"]);
+console.log(hat);
+console.log(hat.name);
+hat.showColors();
+
